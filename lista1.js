f5e8ad2f122ae7b854ef7be7a42b0842013b0cd1
cardozoyuri-jpg <cardozo.yuri@escola.pr.gov.br>
2026-02-26 14:41:28 +0000

    resoluçao da lista

---
+++ lista1.js
@@ -0,0 +1,25 @@
+//criar um programa para exibir a mensagem:"olá,mundo.
+let mensagem= 'olá,mundo';
+console.log(mensagem);
+//criar um programa para exibir seu nome completo
+let nome='meu nome é yuri';
+console.log(nome)
+//criar um programa que mostre 1 numeros e exiba seu dobro
+let numeros=4
+let dobro=numeros*2
+console.log(dobro)
+//criar um programa que mostre 1 numeros e exiba seu triplo
+let numero=6
+let triplo=numeros*3
+console.log(triplo)
+//criar um programa que mostre duas notas e calcule a média
+let nota=10
+let nota2=20
+let media=(nota+nota2)/2
+console.log(media)
+//criar um programa para converter metros em centímetros
+ 
+//criar um programa para mostrar o antecessor e sucessor de um número
+//criar um programa que mostre a área de um retângulo
+//criar um programa para calcular um aumento de 10% num salário de 2000,00
+//criar um programa para verificar se eu número é par ou impar
