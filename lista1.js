5de8e2b90e2ef0dd56de52a11c1b8fc62e94066a
cardozoyuri-jpg <cardozo.yuri@escola.pr.gov.br>
2026-02-27 12:35:43 +0000

    lista2

--- lista1.js
+++ lista1.js
@@ -1,25 +1,63 @@
 //criar um programa para exibir a mensagem:"olá,mundo.
+
 let mensagem= 'olá,mundo';
 console.log(mensagem);
+
 //criar um programa para exibir seu nome completo
+
 let nome='meu nome é yuri';
 console.log(nome)
+
 //criar um programa que mostre 1 numeros e exiba seu dobro
+
 let numeros=4
 let dobro=numeros*2
 console.log(dobro)
+
 //criar um programa que mostre 1 numeros e exiba seu triplo
+
 let numero=6
 let triplo=numeros*3
 console.log(triplo)
+
 //criar um programa que mostre duas notas e calcule a média
+
 let nota=10
 let nota2=20
 let media=(nota+nota2)/2
 console.log(media)
+
 //criar um programa para converter metros em centímetros
- 
+
+ let n3= 4;
+ let n4= (n3*100);
+ console.log(´${metros é igual a ${n4}centimentros.´);
+
 //criar um programa para mostrar o antecessor e sucessor de um número
+
+let n5 =10;
+let n6 = (n5-1);
+let n7 =(n5+1);
+console.log(`o numero antecessor de ${n5} é ${n6}, já o sucessor é ${n7}.`);
+
 //criar um programa que mostre a área de um retângulo
+
+let base = 20
+let altura = 25
+let area = base*altura
+console.log(`A área do rentângulo é ${area}.`);
+
 //criar um programa para calcular um aumento de 10% num salário de 2000,00
+
+let salario = 2000
+let aumento = 2000*1.10
+console.log(`o salario é de 2000 e reais com o aumento ele vai passar a ser ${aumento}.`)
+
 //criar um programa para verificar se eu número é par ou impar
+
+let numero = 89;
+if (numero % 2 === 0){
+    console.log(`numero par!`);
+}else{
+    console.log(`numero impar!`);
+}
