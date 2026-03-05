8f963f30fcdeb3399635e81eef1f3665267fdc76
cardozoyuri-jpg <cardozo.yuri@escola.pr.gov.br>
2026-03-05 11:13:51 -0300

    lista

---
+++ lista3.js
@@ -0,0 +1,31 @@
+// Criar um programa para verificar se o número 12 é divisível por 2 e por 3.
+
+
+// Criar um programa para mostrar um desconto de 12% para produtos acima de R$800.
+
+
+// Criar um programa que mostre os números de 1 a 10 usando laço de repetição.
+
+
+// Criar um programa para mostrar apenas os números pares entre 1 e 20.
+
+
+// Criar um programa para mostrar apenas os números ímpares entre 1 e 20.
+
+
+// Criar um programa para mostrar a tabuada do 5.
+
+
+//Criar um programa para mostrar os números de 1 a 7 usando while.
+
+
+// Criar um programa para mostrar a tabuada do 3 usando while.
+
+
+// Criar um programa para mostrar o fatorial de 5.
+
+
+// Criar um programa para saber se o número 7 é primo.
+
+
+// Criar um programa para calcular a potência de 2³.
